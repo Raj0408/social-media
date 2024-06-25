2d3ff8a33df04a86cd1738093408712d99dd15d5
Raj Kaneriya <rajkaneriya48@gmail.com>
2024-06-25 18:14:35 +0530

    SQLAlc Setup Done

--- main.py
+++ main.py
@@ -1,23 +1,25 @@
 
-from fastapi import FastAPI
+from fastapi import Depends, FastAPI
 from typing import Optional
 from fastapi.params import Body
 from pydantic import BaseModel
 from databases import Database
+from sqlalchemy.orm import Session
+
+from . import models
+from .database.database import SessionLocal, engine
 
 DATABASE_URL = "postgresql://postgres:root@localhost/temp"
-
-
 database = Database(DATABASE_URL)
 
+models.Base.metadata.create_all(bind=engine)
 
-
-try: 
-    conn = database.connect(DATABASE_URL)
-
-except Exception as error:
-
-    print("Error",error)
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
 
 class Posts(BaseModel):
     title :str
@@ -32,11 +34,23 @@ dicts = [{
     "2" : "this is second page", 
 }]
 
+# @app.on_event("startup")
+# async def startup():
+#     try:
+#         await database.connect()
+#     except Exception as error:
+#         print("Error occurred during database connection:", error)
+
+
+@app.get("/sql")
+def sql(db : Session = Depends(get_db)):
+    return {"return " : "2022"}
 
 @app.get("/")
 async def read_root():
-    posts = database.execute(""" SELECT * FROM posts""")
-    return {"posts" : posts}
+    query = "SELECT * FROM posts"
+    posts = await database.fetch_all(query=query)
+    return {"posts" : posts[2]["title"]}
 
 # @app.get("/{idf:str}")
 # def get_id(idf):
@@ -50,12 +64,13 @@ async def create_post(payload : Posts):
     title = payload.title
     content =payload.content
     rating = payload.rating
-    # query = f"INSERT INTO posts(title, content, rating) VALUES ({title}),({content}),({rating})) RETURNING id"
-    # try:
-    #     last_record_id = await database.execute(query=query, values=payload.model_dump())
-    # except:
-    #     print("Error Occured")
-    return {"id": last_record_id, **payload.model_dump()}
+    query = f"INSERT INTO posts(title, content, rating) VALUES ((:title),(:content),(:rating))"
+    try:
+        
+        last_record_id = await database.execute(query=query, values=payload.model_dump())
+    except Exception as error:
+        print("Error Occured" , error)
+    return {"id": "last_record_id"}
     
 
 
---
+++ models.py
@@ -0,0 +1,15 @@
+from sqlalchemy import Boolean, Column, ForeignKey, Integer, String
+from sqlalchemy.orm import relationship
+
+from .database.database import Base
+
+
+class Posts(Base):
+    __tablename__ = "socialpost"
+
+    id = Column(Integer, primary_key=True)
+    title = Column(String, unique=True, index=True)
+    content = Column(String)
+    rating = Column(Integer, default=0)
+
+
